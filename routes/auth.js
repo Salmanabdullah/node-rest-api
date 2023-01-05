a1166537e9998ee62d55dfcb139fc006abc1fef3
Salmanabdullah <salmandcc@gmail.com>
2023-01-05 17:43:30 +0100

    login functionality added

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+//register
 router.post("/register", async (req, res) => {
   try {
     //generate hashed password
@@ -23,4 +24,21 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(err);
+  }
+});
 module.exports = router;

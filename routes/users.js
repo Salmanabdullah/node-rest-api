8ff3751a0df591b8928bad3910a01ca6bbfeaed1
Salmanabdullah <salmandcc@gmail.com>
2023-01-07 03:04:04 +0100

    unfollowing a user

--- routes/users.js
+++ routes/users.js
@@ -78,6 +78,32 @@ router.put("/:id/follow", async (req, res) => {
     res.status(403).json("You can't follow yourself");
   }
 });
+
 //unfollow a user
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const userToUnfollow = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (userToUnfollow.followers.includes(req.body.userId)) {
+        await userToUnfollow.updateOne({
+          $pull: { followers: req.body.userId },
+        });
+        await currentUser.updateOne({ $pull: { following: req.params.id } });
+
+        res.status(200).json("You have unfollowed " + userToUnfollow.username);
+      } else {
+        res
+          .status(404)
+          .json("You are not following " + userToUnfollow.username);
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(400).json("You can't unfollow yourself");
+  }
+});
 
 module.exports = router;

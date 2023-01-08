4c7bd26e9de4f34d254f2d747352caf6be7d787d
Salmanabdullah <salmandcc@gmail.com>
2023-01-08 21:38:17 +0100

    crud of a post

--- routes/posts.js
+++ routes/posts.js
@@ -2,5 +2,44 @@ const router = require("express").Router();
 const Post = require("../models/Post");
 
 //create a post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post has been updated");
+    } else {
+      res.status(400).json("You can edit your post only");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//delete a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("Post has been deleted");
+    } else {
+      res.status(400).json("You can only delete your post");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;

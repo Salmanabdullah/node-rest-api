1ef9bbe0c26451ba15570ce3959ee2559f3591be
Salmanabdullah <salmandcc@gmail.com>
2023-01-08 23:36:37 +0100

    like and dislike a post

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { json } = require("express");
 const Post = require("../models/Post");
 
 //create a post
@@ -41,5 +42,25 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+//like and dislike a post
+router.put("/:id/likes", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("Post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("Post has been disliked");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//get a post
+//get timeline posts
 
 module.exports = router;
